77e30e144fbdcf0f8417c0bd8e381cb0f86b63aa
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-07-29 11:10:17 +0600

    Route Added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ServiceController;
+use App\Http\Controllers\API\BookingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +14,16 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::post('/bookings', [BookingController::class, 'store']);
+    Route::get('/bookings', [BookingController::class, 'index']);
+
+    // Admin Only
+    Route::middleware('admin')->group(function () {
+        Route::post('/services', [ServiceController::class, 'store']);
+        Route::put('/services/{id}', [ServiceController::class, 'update']);
+        Route::delete('/services/{id}', [ServiceController::class, 'destroy']);
+        Route::get('/admin/bookings', [BookingController::class, 'allBookings']);
+    });
 });
