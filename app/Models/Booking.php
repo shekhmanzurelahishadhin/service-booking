c162d0c68404514162bb39483e0495e199788574
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-07-29 09:39:28 +0600

    Model Relation Added

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
 }
