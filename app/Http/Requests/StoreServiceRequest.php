b37f091cdb8086d519ba1467cf99ecb8706cbc3d
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-07-29 10:55:08 +0600

    Service Request added

---
+++ app/Http/Requests/StoreServiceRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Service name is required.',
+            'price.required' => 'Price is required.',
+            'price.numeric' => 'Price must be a number.',
+        ];
+    }
+}
